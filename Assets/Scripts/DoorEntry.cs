a6e635ae44a82e1e5a1911ee93a12d67c6d1f73e
Chris Butler <cbdigitalaspects@gmail.com>
2019-07-26 23:56:50 -0400

    Added Doors & Enhanced Shield Timer
    -Fixed Shield for lasting too long.
    -Synced the cooldown of shield to match trigger box.
    -Added door animation to Level 1 House.

---
+++ Assets/Scripts/DoorEntry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoorEntry : MonoBehaviour
+{
+    public string levelToLoad;
+    public GameObject doorEntry;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorEntry.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            doorEntry.SetActive(true);
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            doorEntry.SetActive(false);
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            if (Input.GetButtonDown("Attack"))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MazePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazePlayer : MonoBehaviour
+{
+    public float maxSpeed = 5;
+    public float speed = 150f;
+    private Rigidbody2D rb2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move left & right controls
+        if (Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            rb2d.AddForce(Vector2.right * speed);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0.1f)
+        {
+            rb2d.AddForce(Vector2.left * speed);
+
+        }
+
+        //Move up and down
+        if (Input.GetKey("Vertical"))
+
+        {
+            rb2d.AddForce(Vector2.up * speed);
+        }
+
+    }
+
+
+}
